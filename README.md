32f3e707b6306019a7fb8d9a873d58d11c6100a2
Koen Klinkers <k.klinkers@gmail.com>
2023-08-16 22:34:23 +0200

    working and added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Logistic map generator
+
+```
+cargo build --release
+./target/release/logistic_map --help
+./target/release/logistic_map 0 4 0 1 --file=fullview.png
+./target/release/logistic_map 3.5 3.8 0 1 --file=chaos.png
+```
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use clap::Parser;
 use plotters::prelude::*;
+use rand::{thread_rng, Rng};
 use std::error::Error;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Start value
-    start: f64,
-    /// End value{
-    end: f64,
+    /// Start value x-axis
+    start_x: f64,
+    /// End value x-axis
+    end_x: f64,
+    /// Start value x-axis
+    start_y: f64,
+    /// End value y-axis
+    end_y: f64,
     #[arg(short, long, default_value = "./output.png")]
     output_file: String,
     #[arg(long, default_value_t = 1000)]
@@ -18,8 +23,10 @@ struct Args {
 fn main() {
     let args = Args::parse();
     graph(
-        args.start,
-        args.end,
+        args.start_x,
+        args.end_x,
+        args.start_y,
+        args.end_y,
         args.width,
         args.height,
         args.output_file,
@@ -27,9 +34,45 @@ fn main() {
     .unwrap();
 }
 
+fn run(r: f64, init: f64, iterations: usize) -> Vec<(f64, f64)> {
+    let mut result = vec![(r, init); iterations];
+    for i in 1..iterations {
+        result[i].1 = r * result[i - 1].1 * (1f64 - result[i - 1].1);
+    }
+    result
+}
+
+fn get_data_set(start: f64, end: f64, width: u32) -> Vec<(f64, f64)> {
+    let mut data_set: Vec<(f64, f64)> = vec![];
+    let step = (end - start) / width as f64;
+    let mut r = start - step;
+    let mut rng = thread_rng();
+    for _ in 0..width {
+        r = r + step;
+        if r < 0.0 || r > 4.0 {
+            break;
+        } else if r < 1.0 {
+            data_set.push((r, 0.))
+        } else if r < 2.0 {
+            let new_run = run(r, rng.gen(), 200);
+            data_set.push(*new_run.last().unwrap());
+        } else if r < 3.0 {
+            let mut new_run = run(r, rng.gen(), 400);
+            data_set.append(&mut new_run.drain(397..).collect::<Vec<_>>());
+        } else {
+            let mut new_run = run(r, rng.gen(), 800);
+            data_set.append(&mut new_run.drain(500..).collect::<Vec<_>>());
+        }
+    }
+
+    data_set
+}
+
 fn graph(
     start: f64,
     end: f64,
+    start_y: f64,
+    end_y: f64,
     width: u32,
     height: u32,
     file_name: String,
@@ -39,12 +82,22 @@ fn graph(
 
     let mut chart = ChartBuilder::on(&root)
         .caption("Bifucation diagram", ("sans-serif", 20))
-        .set_label_area_size(LabelAreaPosition::Left, 100)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
         .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .set_label_area_size(LabelAreaPosition::Right, 100)
-        .build_cartesian_2d((start as isize)..(end as isize), 0..1)?;
+        .set_label_area_size(LabelAreaPosition::Right, 10)
+        .build_cartesian_2d(start..end, start_y..end_y)?;
 
-    chart.configure_mesh().x_desc("r").draw()?;
+    chart.configure_mesh().disable_mesh().x_desc("r").draw()?;
+
+    let data_set = get_data_set(start, end, width);
+
+    chart
+        .draw_series(
+            data_set
+                .iter()
+                .map(|point| Pixel::new(*point, Into::<ShapeStyle>::into(&BLACK).filled())),
+        )
+        .unwrap();
 
     Ok(())
 }
@@ -55,6 +108,6 @@ mod tests {
 
     #[test]
     fn test_basic() {
-        graph(1., 3., 1000, 500, "test.png".to_string()).unwrap();
+        graph(0.0, 4.0, 0.0, 1.0, 100, 50, "test.png".to_string()).unwrap();
     }
 }
